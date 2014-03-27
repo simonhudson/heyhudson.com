36ddb673758bc67bcf3261fe6eece07708b85e5f
Simon Hudson <heysimon@heyhudson.com>
2014-03-27 18:52:04 +0000

    Teaser fade UI

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <?php foreach ($portfolioItems as $portfolioItem) { ?>
 <div class="col span-3 item-teaser-wrap">
 	<div class="item-teaser">
-		<a data-title="<?php echo $portfolioItem['title']; ?>" href="<?php echo $sitePath['root'].'portfolio/'.$portfolioItem['niceurl']; ?>">
-			<img src="<?php echo $sitePath['imgs']; ?>grab-small-<?php echo $portfolioItem['niceurl']; ?>.png" alt="<?php echo $portfolioItem['title']; ?> screen grab" />
-			<span class="item-teaser-title"><?php echo $portfolioItem['title']; ?></span>
-		</a>
-		<!-- <img src="<?php echo $sitePath['imgs']; ?>grab-small-<?php echo $portfolioItem['niceurl']; ?>.png" alt="<?php echo $portfolioItem['title']; ?> screen grab" />
-	    <h2 id="<?php echo $portfolioItem['niceurl']; ?>"><a href="<?php echo $siteRoot.'portfolio/'.$portfolioItem['niceurl']; ?>"><?php echo $portfolioItem['title']; ?></a></h2> -->
-    </div><!--/item-teaser-->
+		<h2>
+			<a href="<?php echo $siteRoot.'portfolio/'.$portfolioItem['niceurl']; ?>">
+				<img src="<?php echo $sitePath['imgs']; ?>grab-small-<?php echo $portfolioItem['niceurl']; ?>.png" alt="<?php echo $portfolioItem['title']; ?> screen grab" />
+				<span class="item-teaser-title"><?php echo $portfolioItem['title']; ?></span>
+			</a>
+		</h2>
+	</div><!--/item-teaser-->
 </div>
 <?php } ?>
 <?php include('includes/tmp_postcontent.inc.php'); ?>
